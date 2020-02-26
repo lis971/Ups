96353f069356e15e238d6465087af39c636ba2ce
Lissandro07 <49215471+Lissandro07@users.noreply.github.com>
2020-02-25 22:02:15 -0300

    Adicionando arquivo "Install.sh"

---
+++ Install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Instalar "updator"
+echo [Iniciando instalação de updator]
+
+# Copiar "updator" para o diretório /bin
+
+sudo cp updator /bin
+
+# Dar permissão para o "updator" ser executado
+
+sudo chmod+ /bin/updator
+
+# Finalizando o processo
+
+echo "Pronto :)"
