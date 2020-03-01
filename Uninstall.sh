71c8e4347911fcda95fa7919e4e906a9ee0e3bb6
Lissandro07 <49215471+Lissandro07@users.noreply.github.com>
2020-03-01 17:01:20 -0300

    Add Uninstall.sh

---
+++ Uninstall.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Desinstalar "ups"
+echo [Iniciando desinstalação de ups]
+
+# Remover "ups" do diretório /bin
+
+echo "[Removendo 'ups' de /bin]"
+sudo rm /bin/ups
+
+# Finalizando o processo
+
+echo "Pronto :)
+Ups foi desinstalado"
